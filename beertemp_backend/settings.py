0657228d5cde770d1c4c0487567625469fdbbb22
Ignacio Jaureguiberry <ijaureguiberry@gmail.com>
2020-07-04 16:02:06 -0300

    1.x to 2.x migration issues.

--- beertemp_backend/settings.py
+++ beertemp_backend/settings.py
@@ -45,7 +45,6 @@ MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -69,9 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
-WSGI_APPLICATION = 'beertemp_backend.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
